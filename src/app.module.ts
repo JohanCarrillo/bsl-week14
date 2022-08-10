ac70ffd1bb69a09265a80b35ce85fed1ed8c5a6b
JohanCarrillo <johanime1907@gmail.com>
2022-08-09 19:40:09 -0500

    added documentation with swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
+// controllers
 import { HealthController } from './health/health.controller';
 import { PetController } from './pet/pet.controller';
+import { AppController } from './app.controller';
+
+// services
+import { AppService } from './app.service';
 import { PetService } from './pet/pet.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'documentation'),
+    }),
+  ],
   controllers: [AppController, HealthController, PetController],
   providers: [AppService, PetService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('BSL-WEEK14-NESTJS-PART2')
+    .setDescription('Tasks of the module')
+    .setVersion('1.0')
+    .addTag('task 4')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const PORT = parseInt(process.env.PORT, 10) || 3000;
   await app.listen(PORT);
 }
